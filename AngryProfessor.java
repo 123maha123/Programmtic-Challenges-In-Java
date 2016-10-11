2fd0dd457635e45bf5e846a3b9ace845d7e15a4f
Mahalakshmi Ramanathan <mahalakshmi@outlook.com>
2016-10-11 17:07:58 +0530

    Create AngryProfessor.java

---
+++ AngryProfessor.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        for(int i=0;i<t;i++){
+            int n=sc.nextInt();
+            int k=sc.nextInt();
+            int count=0;
+            for(int j=0;j<n;j++){
+                if(sc.nextInt()<=0)
+                    count++;
+            }
+            if(count>=k){
+                System.out.println("NO");
+            }else{
+                System.out.println("YES");
+            }
+        }
+    }
+}
